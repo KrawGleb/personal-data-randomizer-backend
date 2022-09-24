1d24d8d2eeb13eb8328ea5d930c42be291df0665
KrawGleb <krawcevitsch@gmail.com>
2022-09-24 21:20:37 +0300

    Add application

---
+++ iLearning.PersonalDataRandomizer.Application/DependencyInjection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace iLearning.PersonalDataRandomizer.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services) 
+    {
+        return services;
+    }
+}
---
+++ iLearning.PersonalDataRandomizer.Domain/Enums/CountriesEnum.cs
@@ -0,0 +1,8 @@
+﻿namespace iLearning.PersonalDataRandomizer.Domain.Enums;
+
+public static class Country
+{
+    public const string Russia = "ru";
+    public const string USA = "us";
+    public const string Poland = "pl";
+}
