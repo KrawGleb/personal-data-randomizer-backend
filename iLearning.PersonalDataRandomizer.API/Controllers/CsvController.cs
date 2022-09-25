eeee4c63286b4db6890f9164541d565127d1188e
KrawGleb <krawcevitsch@gmail.com>
2022-09-26 00:00:15 +0300

    Add csv export

---
+++ iLearning.PersonalDataRandomizer.API/Controllers/CsvController.cs
@@ -0,0 +1,31 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using iLearning.PersonalDataRandomizer.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Text;
+
+namespace iLearning.PersonalDataRandomizer.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CsvController : ControllerBase
+{
+    [HttpPost]
+    public async Task<FileResult> GenerateCsv(IEnumerable<PersonalData> data)
+    {
+        var stringWriter = new StringWriter();
+
+        TextWriter writer = stringWriter;
+
+        var csvWriter = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            Delimiter = ";"
+        });
+
+        await csvWriter.WriteRecordsAsync(data);
+
+        return File(Encoding.UTF8.GetBytes(stringWriter.ToString()), "text/csv", "data.csv");
+    }
+}
